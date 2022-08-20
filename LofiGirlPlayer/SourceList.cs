a5ba8b119248725d9d058c3ef5a637104b5b15f4
FAR747 <far@hightgames.ru>
2022-08-20 19:40:09 +0300

    add volume change

---
+++ LofiGirlPlayer/SourceList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace LofiGirlPlayer
+{
+	public class SourceList
+	{
+		[JsonProperty("version")]
+		public long Version { get; set; }
+
+		[JsonProperty("sources")]
+		public List<SourceListElement> Sources { get; set; }
+	}
+	public partial class SourceListElement
+	{
+		[JsonProperty("Name")]
+		public string Name { get; set; }
+
+		[JsonProperty("YTID")]
+		public string Ytid { get; set; }
+	}
+}
